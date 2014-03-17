dbcd6dc1c77c0ad8f0489a5b0b8b9a6cc97a0807
Zachary Huff <zach.huff.386@gmail.com>
2014-03-16 23:43:20 -0400

    Add daemon client

---
+++ pritunl_client/daemon_client.py
@@ -0,0 +1,108 @@
+from constants import *
+from exceptions import *
+import socket
+import sys
+import json
+import subprocess
+import time
+import threading
+import gtk
+import gobject
+
+_data = {
+    'process': None,
+    'sock': None,
+}
+
+class DaemonClient:
+    def open(self):
+        for i in xrange(5):
+            process = subprocess.Popen(['gksudo', '--description',
+                'Pritunl Client', 'python2 daemon.py %s' % SOCK_PATH],
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            _data['process'] = process
+            thread_data = {
+                'pass_fail': False,
+            }
+
+            def stdout_thread():
+                while True:
+                    line = process.stderr.readline()
+                    if not line:
+                        if process.poll() is not None:
+                            break
+                        else:
+                            continue
+                    if 'incorrect password' in line:
+                        thread_data['pass_fail'] = True
+
+            thread = threading.Thread(target=stdout_thread)
+            thread.daemon = True
+            thread.start()
+
+            while True:
+                if os.path.exists(SOCK_PATH):
+                    break
+                elif process.poll() is not None:
+                    break
+                time.sleep(0.1)
+
+            if process.poll() == 255:
+                raise SudoCancel()
+                return
+
+            if thread_data['pass_fail']:
+                # TODO
+                dialog = gtk.MessageDialog(
+                    type=gtk.MESSAGE_ERROR,
+                    buttons=gtk.BUTTONS_OK,
+                    message_format='Password is incorrect, try again...')
+                dialog.format_secondary_markup(
+                    'Failed to obtain sudo privileges')
+                dialog.set_title('Pritunl - Incorrect Password')
+                dialog.show_all()
+                dialog.run()
+                dialog.destroy()
+                continue
+
+            break
+
+        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        sock.connect(SOCK_PATH)
+        _data['sock'] = sock
+
+    def close(self):
+        sock = _data.get('sock')
+        if not sock:
+            sock.close()
+
+    def send(self, data):
+        sock = _data.get('sock')
+        if not sock:
+            self.open()
+        sock = _data.get('sock')
+        if not sock:
+            return
+        sock.sendall(json.dumps(data))
+
+    def start_conn(self, id, working_dir, path, log_path):
+        self.send({
+            'cmd': 'start',
+            'id': id,
+            'working_dir': working_dir,
+            'path': path,
+            'log_path': log_path,
+        })
+
+    def stop_conn(self, id):
+        self.send({
+            'cmd': 'stop',
+            'id': id,
+        })
+
+    def exit(self):
+        if not _data.get('sock'):
+            return
+        self.send({
+            'cmd': 'exit',
+        })

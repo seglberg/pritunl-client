bb32fc5c96027de87bea58ce2ce3450c728a8bab
Zachary Huff <zach.huff.386@gmail.com>
2014-03-17 04:50:23 -0400

    Add info to init

--- pritunl_client/__init__.py
+++ pritunl_client/__init__.py
@@ -1,2 +1,9 @@
+from constants import *
+__title__ = 'pritunl_client'
+__version__ = '0.1.0'
+__author__ = 'Zachary Huff'
+__license__ = 'AGPL'
+__copyright__ = 'Copyright 2013-2014 Zachary Huff'
+
 from interface import Interface
 from daemon import Daemon

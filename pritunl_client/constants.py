0467fde9004a13c1f7c2dd68254b0f74842ca6e4
Zachary Huff <zach.huff.386@gmail.com>
2014-03-16 23:09:06 -0400

    Add constants

--- pritunl_client/constants.py
+++ pritunl_client/constants.py
@@ -1,6 +1,11 @@
 import os
+import uuid
 
 CONF_DIR = os.path.expanduser('~/.config/pritunl')
 PROFILES_DIR = os.path.join(CONF_DIR, 'profiles')
+LOGS_DIR = os.path.join(CONF_DIR, 'logs')
+SOCK_PATH = '/tmp/pritunl_%s.sock' % uuid.uuid4().hex
 CONNECT_TIMEOUT = 30
-OVPN_EXIT_TIMEOUT = 5
+OVPN_START_TIMEOUT = 5
+OVPN_STOP_TIMEOUT = 5
+DAEMON_SOCKET_TIMEOUT = 10

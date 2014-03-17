87b01a45bae7f063bcfa04eca9e60ce878329e73
Zachary Huff <zach.huff.386@gmail.com>
2014-03-16 23:44:18 -0400

    Add linux start/stop to profile

--- pritunl_client/profile.py
+++ pritunl_client/profile.py
@@ -1,4 +1,6 @@
 from constants import *
+from exceptions import *
+from daemon_client import DaemonClient
 import os
 import uuid
 import json
@@ -6,6 +8,7 @@ import time
 import subprocess
 import threading
 import signal
+import socket
 import gtk
 import gobject
 
@@ -26,7 +29,12 @@ class Profile:
         if not os.path.isdir(PROFILES_DIR):
             os.makedirs(PROFILES_DIR)
 
+        if not os.path.isdir(LOGS_DIR):
+            os.makedirs(LOGS_DIR)
+
+        self.working_dir = PROFILES_DIR
         self.path = os.path.join(PROFILES_DIR, '%s.ovpn' % self.id)
+        self.log_path = os.path.join(LOGS_DIR, '%s.log' % self.id)
 
         if id:
             self.load()
@@ -37,8 +45,8 @@ class Profile:
         elif name == 'status':
             connection_data = _connections.get(self.id)
             if connection_data:
-                return connection_data.get('status', False)
-            return False
+                return connection_data.get('status', ENDED)
+            return ENDED
         elif name not in self.__dict__:
             raise AttributeError('Config instance has no attribute %r' % name)
         return self.__dict__[name]
@@ -62,30 +70,53 @@ class Profile:
                 self.server_name = info_data['server']
 
     def write(self, data):
+        data += '\nlog-append ../logs/%s.log' % self.id
         with open(self.path, 'w') as profile_file:
             profile_file.write(data)
 
+    def _set_status(self, status):
+        data = _connections.get(self.id)
+        if not data:
+            return
+        data['status'] = status
+        callback = data.get('dialog_callback')
+        if callback:
+            data['dialog_callback'] = None
+            gobject.idle_add(callback)
+
+        callback = data.get('status_callback')
+        if callback:
+            gobject.idle_add(callback)
+
     def start(self, status_callback, dialog_callback):
-        process = subprocess.Popen(['gksudo', '--description',
-            'Pritunl Connect', 'openvpn %s' % self.path],
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         data = {
-            'status': False,
-            'process': process,
+            'status': CONNECTING,
+            'process': None,
             'status_callback': status_callback,
             'dialog_callback': dialog_callback,
         }
         _connections[self.id] = data
 
-        def change_status(status):
-            data['status'] = status
-            if data['dialog_callback']:
-                callback = data['dialog_callback']
-                data['dialog_callback'] = None
-                gobject.idle_add(callback, self)
-            gobject.idle_add(data['status_callback'], self)
+        client = DaemonClient()
+        try:
+            client.start_conn(self.id, self.working_dir,
+                self.path, self.log_path)
+        except SudoCancel:
+            data['status'] = ENDED
+            return
+
+        for i in xrange(int(OVPN_START_TIMEOUT / 0.1)):
+            time.sleep(0.1)
+            if os.path.exists(self.log_path):
+                break
+        if not os.path.exists(self.log_path):
+            self.stop()
 
         def poll_thread():
+            process = subprocess.Popen(['tail', '-c', '+1',
+                '--follow=name', self.log_path], stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE)
+            data['process'] = process
             while True:
                 line = process.stdout.readline()
                 if not line:
@@ -95,16 +126,14 @@ class Profile:
                         continue
                 print line.strip()
                 if 'Initialization Sequence Completed' in line:
-                    change_status(True)
+                    self._set_status(CONNECTED)
                 elif 'Inactivity timeout' in line:
-                    change_status(False)
-            print 'EXIT_OVPN'
-            data.pop(self.id, None)
-            change_status(False)
+                    self._set_status(RECONNECTING)
+            self._set_status(DISCONNECTED)
 
         def dialog_thread():
             time.sleep(CONNECT_TIMEOUT)
-            if not data['dialog_callback']:
+            if not data.get('dialog_callback'):
                 return
             self.stop()
 
@@ -117,81 +146,16 @@ class Profile:
         thread.start()
 
     def stop(self):
-        connection_data = _connections.get(self.id)
-        if not connection_data:
-            return
+        client = DaemonClient()
+        client.stop_conn(self.id)
 
-        process = connection_data.get('process')
-        if not process:
-            return
+        data = _connections.get(self.id)
+        if data:
+            process = data.get('process')
+            if process:
+                process.kill()
 
-        # TODO run in thread
-        for i in xrange(10):
-            kill_process = subprocess.Popen(['gksudo', '--description',
-                'Pritunl Disconnect', 'pkill -P %s' % process.pid],
-                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            pass_fail = False
-
-            while True:
-                line = kill_process.stderr.readline()
-                if not line:
-                    if kill_process.poll() is not None:
-                        break
-                    else:
-                        continue
-                if 'incorrect password' in line:
-                    pass_fail = True
-
-            if kill_process.poll() == 255:
-                return
-
-            if pass_fail:
-                dialog = gtk.MessageDialog(
-                    type=gtk.MESSAGE_ERROR,
-                    buttons=gtk.BUTTONS_OK,
-                    message_format='Password is incorrect, try again...')
-                dialog.format_secondary_markup(
-                    'Failed to obtain sudo privileges')
-                dialog.set_title('Pritunl - Incorrect Password')
-                dialog.show_all()
-                dialog.run()
-                dialog.destroy()
-                continue
-
-            for i in xrange(int(OVPN_EXIT_TIMEOUT / 0.1)):
-                time.sleep(0.1)
-                if process.poll() is not None:
-                    break
-
-            break
-
-        if process.poll() is None:
-            dialog = gtk.MessageDialog(
-                type=gtk.MESSAGE_ERROR,
-                buttons=gtk.BUTTONS_OK,
-                message_format='Unable to disconnect profile')
-            dialog.format_secondary_markup(
-                'Failed to stop profile connection process')
-            dialog.set_title('Pritunl - Disconnect Failed')
-            dialog.show_all()
-            dialog.run()
-            dialog.destroy()
-
-
-        # for i in xrange(int(OVPN_EXIT_TIMEOUT / 0.1)):
-        #     time.sleep(0.1)
-        #     if process.poll() is not None:
-        #         break
-        #     process.terminate()
-        # if process.poll() is not None:
-        #     return
-
-        # process.kill()
-        # for i in xrange(int(OVPN_EXIT_TIMEOUT / 0.1)):
-        #     time.sleep(0.1)
-        #     if process.poll() is not None:
-        #         break
-        #     process.kill()
+        self._set_status(ENDED)
 
     @classmethod
     def iter_profiles(cls):

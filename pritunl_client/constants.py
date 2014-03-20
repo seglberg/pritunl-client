735a638e6f92fb880ba7c9507dd172887f8ce7cb
Zachary Huff <zach.huff.386@gmail.com>
2014-03-20 03:14:40 -0400

    Change img paths

--- pritunl_client/constants.py
+++ pritunl_client/constants.py
@@ -25,14 +25,36 @@ OVPN_START_TIMEOUT = 5
 OVPN_STOP_TIMEOUT = 5
 DAEMON_SOCKET_TIMEOUT = 10
 
-IMG_ROOT = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), 'img')
-LOGO = os.path.join(IMG_ROOT, 'logo.png')
-if PLATFORM == WIN:
-    CONNECTED_LOGO = os.path.join(IMG_ROOT, 'logo_connected_win.png')
-    DISCONNECTED_LOGO = os.path.join(IMG_ROOT, 'logo_disconnected_win.png')
+LOGO = None
+CONNECTED_LOGO = None
+DISCONNECTED_LOGO = None
+IMG_ROOTS = [
+    os.path.join(os.path.abspath(os.sep), 'usr', 'share', 'pritunl'),
+    os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), 'img'),
+]
+
+if PLATFORM == LINUX:
+    _CONNECTED_LOGO_NAME = 'logo_connected_light.svg'
+    _DISCONNECTED_LOGO_NAME = 'logo_disconnected_light.svg'
+elif PLATFORM == WIN:
+    _CONNECTED_LOGO_NAME = 'logo_connected_win.png'
+    _DISCONNECTED_LOGO_NAME = 'logo_connected_win.png'
+elif PLATFORM == OSX:
+    _CONNECTED_LOGO_NAME = 'logo_connected_osx.png'
+    _DISCONNECTED_LOGO_NAME = 'logo_connected_osx.png'
 else:
-    CONNECTED_LOGO = os.path.join(IMG_ROOT, 'logo_connected_light.svg')
-    DISCONNECTED_LOGO = os.path.join(IMG_ROOT, 'logo_disconnected_light.svg')
+    raise NotImplementedError('Platform not supported')
+
+for img_root in IMG_ROOTS:
+    img_path = os.path.join(img_root, 'logo.png')
+    if os.path.exists(img_path) and not LOGO:
+        LOGO = img_path
+    img_path = os.path.join(img_root, _CONNECTED_LOGO_NAME)
+    if os.path.exists(img_path) and not CONNECTED_LOGO:
+        CONNECTED_LOGO = img_path
+    img_path = os.path.join(img_root, _DISCONNECTED_LOGO_NAME)
+    if os.path.exists(img_path) and not DISCONNECTED_LOGO:
+        DISCONNECTED_LOGO = img_path
 
 SUDO_PASS_FAIL = 'sudo_pass_fail'
 CONNECTING = 'connecting'

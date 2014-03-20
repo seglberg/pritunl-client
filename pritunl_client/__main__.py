021bd61fb5b0311f4f99a68145f4bd2f50270a56
Zachary Huff <zach.huff.386@gmail.com>
2014-03-20 02:40:36 -0400

    Add pk to main

--- pritunl_client/__main__.py
+++ pritunl_client/__main__.py
@@ -2,9 +2,11 @@ from constants import *
 import optparse
 import sys
 import os
-import pritunl_client
+import re
+import subprocess
 
 def client():
+    import pritunl_client.interface
     parser = optparse.OptionParser()
     parser.add_option('--version', action='store_true', help='Print version')
     (options, args) = parser.parse_args()
@@ -13,5 +15,16 @@ def client():
         print '%s v%s' % (pritunl.__title__, pritunl.__version__)
         sys.exit(0)
 
-    interface = pritunl_client.Interface()
+    interface = pritunl_client.interface.Interface()
     interface.main()
+
+def pk():
+    if sys.argv[1] == 'stop':
+        regex = r'(?:/pritunl/profiles/[a-z0-9]+\.ovpn)$'
+        with open('/proc/%s/cmdline' % sys.argv[2], 'r') as cmdline_file:
+            cmdline = cmdline_file.read().strip('\x00')
+            if not re.search(regex, cmdline):
+                raise ValueError('Not a pritunl client process')
+        subprocess.check_call(['kill', sys.argv[2]])
+    elif sys.argv[1] == 'copy':
+        pass

ad84bfba3b2a58379e2192ecd87b17d6d7f51ab4
Zachary Huff <zach.huff.386@gmail.com>
2014-04-25 07:56:46 -0400

    Move win admin check

--- win_main.py
+++ win_main.py
@@ -1,13 +1,6 @@
 import optparse
 import sys
-import os
 import pritunl_client.app
-import ctypes
-import win32con
-import win32event
-import win32process
-from win32com.shell import shell
-from win32com.shell import shellcon
 
 parser = optparse.OptionParser()
 parser.add_option('--version', action='store_true', help='Print version')
@@ -16,23 +9,3 @@ parser.add_option('--version', action='store_true', help='Print version')
 if options.version:
     print '%s v%s' % (pritunl.__title__, pritunl.__version__)
     sys.exit(0)
-
-try:
-    is_admin = ctypes.windll.shell32.IsUserAnAdmin()
-except:
-    is_admin = False
-
-if not is_admin:
-    cmd = '"%s"' % sys.executable
-    params = ' '.join(['"%s"' % x for x in sys.argv])
-
-    proc_info = shell.ShellExecuteEx(nShow=win32con.SW_SHOWNORMAL,
-        fMask=shellcon.SEE_MASK_NOCLOSEPROCESS,
-        lpVerb='runas', lpFile=cmd, lpParameters=params)
-
-    proc_handle = proc_info['hProcess']
-    obj = win32event.WaitForSingleObject(proc_handle, win32event.INFINITE)
-    rc = win32process.GetExitCodeProcess(proc_handle)
-else:
-    app = pritunl_client.app.App()
-    app.main()

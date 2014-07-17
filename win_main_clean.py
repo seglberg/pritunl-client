95ac69903c25f3d46c02d078796f71ed91693138
Zachary Huff <zach.huff.386@gmail.com>
2014-07-17 00:17:25 -0400

    Run win main clear as admin

--- win_main_clean.py
+++ win_main_clean.py
@@ -1,2 +1,24 @@
 import pritunl_client.profile_win
+import sys
+import ctypes
+import win32con
+import win32event
+import win32process
+from win32com.shell import shell
+from win32com.shell import shellcon
+
+try:
+    is_admin = ctypes.windll.shell32.IsUserAnAdmin()
+except:
+    is_admin = False
+
+if not is_admin:
+    cmd = '"%s"' % sys.executable
+    params = ' '.join(['"%s"' % x for x in sys.argv])
+
+    proc_info = shell.ShellExecuteEx(nShow=win32con.SW_SHOWNORMAL,
+        fMask=shellcon.SEE_MASK_NOCLOSEPROCESS,
+        lpVerb='runas', lpFile=cmd, lpParameters=params)
+    sys.exit(0)
+
 pritunl_client.profile_win.ProfileWin.clear_tap_adapters()

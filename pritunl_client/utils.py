42e75d9cfd14e53ff928e4109c5d17fa0c1e8404
Zachary Huff <zach.huff.386@gmail.com>
2014-07-20 04:27:26 -0400

    Add request to utils

--- pritunl_client/utils.py
+++ pritunl_client/utils.py
@@ -1,5 +1,78 @@
 from constants import *
 import interface
+import urllib2
+import httplib
+import socket
+import json
+
+class Response:
+    def __init__(self, url, headers, status_code, reason, content):
+        self.url = url
+        self.headers = headers
+        self.status_code = status_code
+        self.reason = reason
+        self.content = content
+
+    def json(self):
+        return json.loads(self.content)
+
+class request:
+    @classmethod
+    def _request(cls, method, url, json_data=None, headers={},
+            timeout=socket._GLOBAL_DEFAULT_TIMEOUT):
+        data = None
+        request = urllib2.Request(url, headers=headers)
+        request.get_method = lambda: method
+
+        if json_data is not None:
+            request.add_header('Content-Type', 'application/json')
+            data = json.dumps(json_data)
+
+        try:
+            url_response = urllib2.urlopen(request, data=data, timeout=timeout)
+            return Response(url,
+                headers=dict(url_response.info().items()),
+                status_code=url_response.getcode(),
+                reason='OK',
+                content=url_response.read(),
+            )
+        except urllib2.HTTPError as error:
+            return Response(url,
+                headers=dict(error.info().items()),
+                status_code=error.getcode(),
+                reason=error.reason,
+                content=error.read(),
+            )
+        except Exception as error:
+            raise httplib.HTTPException(error)
+
+    @classmethod
+    def get(cls, url, **kwargs):
+        return cls._request('GET', url, **kwargs)
+
+    @classmethod
+    def options(cls, url, **kwargs):
+        return cls._request('OPTIONS', url, **kwargs)
+
+    @classmethod
+    def head(cls, url, **kwargs):
+        return cls._request('HEAD', url, **kwargs)
+
+    @classmethod
+    def post(cls, url, **kwargs):
+        return cls._request('POST', url, **kwargs)
+
+    @classmethod
+    def put(cls, url, **kwargs):
+        return cls._request('PUT', url, **kwargs)
+
+    @classmethod
+    def patch(cls, url, **kwargs):
+        return cls._request('PATCH', url, **kwargs)
+
+    @classmethod
+    def delete(cls, url, **kwargs):
+        return cls._request('DELETE', url, **kwargs)
 
 def get_logo():
     if PLATFORM == LINUX:

64d86d1e2eb440095767c8a13e81b098f74e5b01
Zachary Huff <zach.huff.386@gmail.com>
2014-12-13 02:03:02 -0500

    Add exception for platform error

--- pritunl_client/interface.py
+++ pritunl_client/interface.py
@@ -5,3 +5,5 @@ if PLATFORM == LINUX:
     from interface_gtk import *
 elif PLATFORM in (WIN, OSX):
     from interface_wx import *
+else:
+    raise TypeError('Interface not available for platform')

42a11141612866bc799792a7459e0f013d60e67e
Zachary Huff <zach.huff.386@gmail.com>
2014-12-13 01:49:49 -0500

    Add pk commands to main

--- pritunl_client/__main__.py
+++ pritunl_client/__main__.py
@@ -155,15 +155,68 @@ def _pk_start(autostart=False):
     if autostart:
         with open(sys.argv[1], 'r') as profile_file:
             profile_hash = hashlib.sha1(profile_file.read()).hexdigest()
-        etc_dir = os.path.join(os.path.abspath(os.sep),
-            'etc', 'pritunl_client')
-        if not os.path.exists(etc_dir):
-            os.makedirs(etc_dir)
-        profile_hash_path = os.path.join(etc_dir, profile_hash)
-        with open(profile_hash_path, 'w') as profile_hash_file:
-            pass
-    elif sys.argv[1] == 'clear_autostart':
         profile_hash_path = os.path.join(os.path.abspath(os.sep),
-            'etc', 'pritunl_client', sys.argv[2])
-        if os.path.exists(profile_hash_path):
-            os.remove(profile_hash_path)
+            'etc', 'pritunl_client', profile_hash)
+        if not os.path.exists(profile_hash_path):
+            raise ValueError('Profile not authorized to autostart')
+
+    args = ['openvpn', '--config', sys.argv[1]]
+
+    if len(sys.argv) > 2:
+        os.chown(sys.argv[2], os.getuid(), os.getgid())
+        args.append('--auth-user-pass')
+        args.append(sys.argv[2])
+
+    try:
+        process = subprocess.Popen(args)
+        def sig_handler(signum, frame):
+            process.send_signal(signum)
+        signal.signal(signal.SIGINT, sig_handler)
+        signal.signal(signal.SIGTERM, sig_handler)
+        sys.exit(process.wait())
+    finally:
+        if len(sys.argv) > 2:
+            os.remove(sys.argv[2])
+
+def pk_start():
+    _pk_start(False)
+
+def pk_autostart():
+    _pk_start(True)
+
+def pk_stop():
+    pid = int(sys.argv[1])
+    cmdline_path = '/proc/%s/cmdline' % pid
+    regex = r'/pritunl_client/profiles/[a-z0-9]+\.ovpn'
+    if not os.path.exists(cmdline_path):
+        return
+    with open('/proc/%s/cmdline' % pid, 'r') as cmdline_file:
+        cmdline = cmdline_file.read().strip().strip('\x00')
+        if not re.search(regex, cmdline):
+            raise ValueError('Not a pritunl client process')
+    os.kill(pid, signal.SIGTERM)
+    for i in xrange(int(5 / 0.1)):
+        time.sleep(0.1)
+        if not os.path.exists('/proc/%s' % pid):
+            break
+        os.kill(pid, signal.SIGTERM)
+
+def pk_set_autostart():
+    regex = r'(?:/pritunl_client/profiles/[a-z0-9]+\.ovpn)$'
+    if not re.search(regex, sys.argv[1]):
+        raise ValueError('Profile must be in home directory')
+    with open(sys.argv[1], 'r') as profile_file:
+        profile_hash = hashlib.sha1(profile_file.read()).hexdigest()
+    etc_dir = os.path.join(os.path.abspath(os.sep),
+        'etc', 'pritunl_client')
+    if not os.path.exists(etc_dir):
+        os.makedirs(etc_dir)
+    profile_hash_path = os.path.join(etc_dir, profile_hash)
+    with open(profile_hash_path, 'w') as profile_hash_file:
+        pass
+
+def pk_clear_autostart():
+    profile_hash_path = os.path.join(os.path.abspath(os.sep),
+        'etc', 'pritunl_client', sys.argv[1])
+    if os.path.exists(profile_hash_path):
+        os.remove(profile_hash_path)

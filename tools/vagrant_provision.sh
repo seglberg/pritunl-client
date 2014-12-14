9f25a02c4de0da3cd741be4485e3a40c58736463
Zachary Huff <zach.huff.386@gmail.com>
2014-12-13 21:04:37 -0500

    Move vagrant script to vagrant file

--- tools/vagrant_provision.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-apt-get update -qq 1> /dev/null
-
-# Build requirements
-apt-get install -qq -y devscripts debhelper python-all python-setuptools python-gtk2 python-appindicator 1> /dev/null

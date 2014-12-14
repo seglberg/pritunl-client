51a3cd45c6cf74fd2fa6f2bcd67aab81b6feb5fa
Zachary Huff <zach.huff.386@gmail.com>
2014-12-14 16:40:19 -0500

    Add services to setup

--- setup.py
+++ setup.py
@@ -1,43 +1,90 @@
-from pritunl_client.constants import *
 from setuptools import setup
 import os
-import pritunl_client
+import sys
+import copy
+import shutil
+import fileinput
+import shlex
 
+VERSION = '1.0.0'
+PATCH_DIR = 'build'
+install_upstart = True
+install_systemd = True
+install_gtk = True
 data_files = []
 
-if PLATFORM == LINUX:
-    for img_size in os.listdir(os.path.join('img', 'hicolor')):
-        for img_name in os.listdir(os.path.join('img', 'hicolor', img_size)):
-            data_files.append((os.path.join(os.path.abspath(os.sep), 'usr',
-                'share', 'icons', 'hicolor', img_size, 'apps'), [os.path.join(
-                'img', 'hicolor', img_size, img_name)]))
+LINUX = 'linux'
+SHELL = 'shell'
+WIN = 'win'
+OSX = 'osx'
 
-    for img_theme in ('ubuntu-mono-dark', 'ubuntu-mono-light'):
-        for img_size in os.listdir(os.path.join('img', img_theme)):
-            for img_name in os.listdir(os.path.join('img', img_theme,
+if sys.platform.startswith('linux'):
+    PLATFORM = LINUX
+elif sys.platform == 'win32':
+    PLATFORM = WIN
+elif sys.platform == 'darwin':
+    PLATFORM = OSX
+
+prefix = sys.prefix
+for arg in copy.copy(sys.argv):
+    if arg.startswith('--prefix'):
+        prefix = os.path.normpath(shlex.split(arg)[0].split('=')[-1])
+    elif arg == '--no-upstart':
+        sys.argv.remove('--no-upstart')
+        install_upstart = False
+    elif arg == '--no-systemd':
+        sys.argv.remove('--no-systemd')
+        install_systemd = False
+    elif arg == '--no-gtk':
+        sys.argv.remove('--no-gtk')
+        install_gtk = False
+
+if not os.path.exists('build'):
+    os.mkdir('build')
+
+if PLATFORM == LINUX:
+    if install_gtk:
+        for img_size in os.listdir(os.path.join('img', 'hicolor')):
+            for img_name in os.listdir(os.path.join('img', 'hicolor',
                     img_size)):
                 data_files.append((os.path.join(os.path.abspath(os.sep), 'usr',
-                    'share', 'icons', img_theme, 'apps', img_size), [
-                    os.path.join('img', img_theme, img_size, img_name)]))
+                    'share', 'icons', 'hicolor', img_size, 'apps'), [
+                        os.path.join('img', 'hicolor', img_size, img_name)]))
+
+        for img_theme in ('ubuntu-mono-dark', 'ubuntu-mono-light'):
+            for img_size in os.listdir(os.path.join('img', img_theme)):
+                for img_name in os.listdir(os.path.join('img', img_theme,
+                        img_size)):
+                    data_files.append((os.path.join(os.path.abspath(os.sep),
+                        'usr', 'share', 'icons', img_theme, 'apps', img_size),
+                        [os.path.join('img', img_theme, img_size, img_name)]))
+
+        data_files += [
+            (os.path.join(os.path.abspath(os.sep), 'usr', 'share',
+                    'pritunl_client'), [
+                os.path.join('img', 'logo.png'),
+                os.path.join('img', 'logo_connected_dark.svg'),
+                os.path.join('img', 'logo_disconnected_dark.svg'),
+                os.path.join('img', 'logo_connected_light.svg'),
+                os.path.join('img', 'logo_disconnected_light.svg'),
+            ]),
+            (os.path.join(os.path.abspath(os.sep), 'usr', 'share',
+                'applications'), [os.path.join(
+                    'data', 'linux', 'applications',
+                    'pritunl-client-gtk.desktop')]),
+            (os.path.join(os.path.abspath(os.sep), 'etc', 'xdg', 'autostart'),
+                [os.path.join('data', 'linux', 'applications',
+                    'pritunl-client-gtk.desktop')]),
+            (os.path.join(os.path.abspath(os.sep), 'usr', 'share', 'polkit-1',
+                'actions'), [os.path.join('data', 'linux', 'polkit',
+                'com.pritunl.client.policy')]),
+        ]
 
     data_files += [
-        (os.path.join(os.path.abspath(os.sep), 'usr', 'share',
-                'pritunl_client'), [
-            os.path.join('img', 'logo.png'),
-            os.path.join('img', 'logo_connected_dark.svg'),
-            os.path.join('img', 'logo_disconnected_dark.svg'),
-            os.path.join('img', 'logo_connected_light.svg'),
-            os.path.join('img', 'logo_disconnected_light.svg'),
+        (os.path.join(os.path.abspath(os.sep), 'var', 'log'), [
+            os.path.join('data', 'var', 'pritunl-client.log'),
+            os.path.join('data', 'var', 'pritunl-client.log.1'),
         ]),
-        (os.path.join(os.path.abspath(os.sep), 'usr', 'share',
-            'applications'), [os.path.join(
-            'data', 'linux', 'applications', 'pritunl-client.desktop')]),
-        (os.path.join(os.path.abspath(os.sep), 'etc', 'xdg', 'autostart'),
-            [os.path.join('data', 'linux', 'applications',
-                'pritunl_client.desktop')]),
-        (os.path.join(os.path.abspath(os.sep), 'usr', 'share', 'polkit-1',
-            'actions'), [os.path.join('data', 'linux', 'polkit',
-            'com.pritunl.client.policy')]),
     ]
 
 elif PLATFORM == WIN:

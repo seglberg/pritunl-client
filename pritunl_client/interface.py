009f5e2f903df45fb8b031907cdbed3d42c01d90
Zachary Huff <zach.huff.386@gmail.com>
2015-06-25 03:45:38 -0400

    Update interface init

--- pritunl_client/interface.py
+++ pritunl_client/interface.py
@@ -3,7 +3,5 @@ from pritunl_client.exceptions import *
 
 if PLATFORM == LINUX:
     from interface_gtk import *
-elif PLATFORM in (WIN, OSX):
-    from interface_wx import *
 else:
     raise TypeError('Interface not available for platform')

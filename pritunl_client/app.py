3dfa148dd8f95dbb2cbf6c822e156485a5e3311f
Zachary Huff <zach.huff.386@gmail.com>
2015-06-25 03:45:01 -0400

    Remove win code in app

--- pritunl_client/app.py
+++ pritunl_client/app.py
@@ -8,33 +8,9 @@ from pritunl_client import interface
 import threading
 import time
 import sys
-import tarfile
-import httplib
 
 class App(object):
     def __init__(self):
-        if PLATFORM == WIN:
-            import ctypes
-            import win32con
-            import win32event
-            import win32process
-            from win32com.shell import shell
-            from win32com.shell import shellcon
-
-            try:
-                is_admin = ctypes.windll.shell32.IsUserAnAdmin()
-            except:
-                is_admin = False
-
-            if not is_admin:
-                cmd = '"%s"' % sys.executable
-                params = ' '.join(['"%s"' % x for x in sys.argv])
-
-                proc_info = shell.ShellExecuteEx(nShow=win32con.SW_SHOWNORMAL,
-                    fMask=shellcon.SEE_MASK_NOCLOSEPROCESS,
-                    lpVerb='runas', lpFile=cmd, lpParameters=params)
-                sys.exit(0)
-
         self._icon_state = None
         self._cur_icon_path = None
         self.icon = interface.StatusIconApp()
